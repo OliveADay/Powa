33540132b0c31d22b3142d84bf46877f5ef09d68
OliveADay <sharinggon23@gmail.com>
2025-04-10 17:58:28 -0700

    s

--- grapple.gd
+++ grapple.gd
@@ -4,7 +4,7 @@ var shoot_pressed = false
 var grappleArm: Node2D
 var timeafterRelease = 0.3
 var currentTimeAfter = 0
-
+var inwall = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	grappleArm = get_tree().get_first_node_in_group("grappleArm")
@@ -30,6 +30,10 @@ func _physics_process(delta: float) -> void:
 		freeze  = false
 		$grapplegrabber.monitoring = true
 		gravity_scale = 1
+	if linear_velocity.y > 300:
+		gravity_scale = 0
+	elif linear_velocity.y > 0:
+		gravity_scale = 1
 	apply_central_force(force)
 	
 
@@ -37,7 +41,6 @@ func _physics_process(delta: float) -> void:
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group('ground'):
 		freeze = true
-		gravity_scale = 1 # Replace with function body.
 
 
 func _on_grapplegrabber_body_entered(body: Node2D) -> void:
@@ -47,4 +50,5 @@ func _on_grapplegrabber_body_entered(body: Node2D) -> void:
 		$grapplegrabber.monitoring = false
 		position = Vector2(0,0)
 		gravity_scale = 0
+		linear_velocity = Vector2(0,0)
 		rotation = 0
